09fa96fce9fbd3b3eb90b884db930954f3e1f379
ataru <ataru4101826514@gmail.com>
2022-04-14 11:56:07 +0900

    fix secretkey

--- main/main/local_settings.py
+++ main/main/local_settings.py
@@ -4,4 +4,4 @@ secret_key = get_random_secret_key()
 text = 'SECRET_KEY = \'{0}\''.format(secret_key)
 print(text)
 
-SECRET_KEY = '7jd+9+u4$&oc3mv4ppersioog-w+rzzua*%fhek_-76(agf0i!'
+SECRET_KEY = '9l%j$12+)#0ij(6#gm1-xv(7k303fqb70u4k^dys388z#97-+z'
